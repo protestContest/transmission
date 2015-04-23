fadffa4ca1990c3872958ba17ef2025fb05737ac
Zack Michener <zack@zackmichener.net>
2015-04-23 17:01:01 +0000

    move more of the transcript into jade templates

--- app/lib/socketgame.js
+++ app/lib/socketgame.js
@@ -42,14 +42,14 @@ function respond(socket, data) {
       switch (data.text) {
         case "help":
           socket.send({
-            text: "<p>You are connected to the TCS D8303 Scientific Computer Network.<br> You are logged on as Zack</p> <p>Warning: Your account has been limited to essential operations. See your administrator for assistance.</p> <p>Commands:</p> <code>exit  info  logs  messages</code>",
+            text: jade.renderFile(__dirname + '/text/help.jade'),
             request: "command"
           });
         break;
 
         case "info":
           socket.send({
-            text: "<p>You are connected to a TCS D8303 Scientific Computer Network.</p> <p>The D8303 SCN is a state-of-the-art computer system by Tracon Computer Systems. It uses the latest in computing research to harness the power of quantum physics, allowing researchers to solve yet-unsolvable classes of scientific problems.</p> <p>This installation of the D8303 SCN is licensed to:</p> <pre>  INDUSTRIAL SCIENCE INC.</pre> ",
+            text: jade.renderFile(__dirname + '/text/info.jade'),
             request: "command"
           });
         break;
